2437f209e5b81c371632a6ccf3d7b4d651a2e897
Michael Fazio <mfazio@skylinetechnologies.com>
2016-06-08 13:07:25 -0500

    Remove solutions.

--- challenges/challenges/jquery-advanced-selectors.js
+++ challenges/challenges/jquery-advanced-selectors.js
@@ -1,20 +1,20 @@
 module.exports = {
     getInnerLinkText: function(){
-        return $(".inner-a").find('a').text();
+
     },
     getSecondDivText: function(){
-        return $(".class-div").last().text();
+
     },
     getTextFromFirstSpan: function(){
-        return $(".multiple-spans").find('span').first().text();
+
     },
     getTextFromThirdSpan: function(){
-        return $(".multiple-spans").find("span:eq(2)").text();
+
     },
     getEmbeddedLinkText: function(){
-        return $(".embedded").find('a').text();
+
     },
     getFourLevelsDownText: function(){
-        return $(".embedded").find('span').last().text();
+        
     }
 };
--- challenges/challenges/jquery-selectors.js
+++ challenges/challenges/jquery-selectors.js
@@ -1,15 +1,15 @@
 var selectors = {
     getIDDivValue: function() {
-        return $("#id-div").text();
+        
     },
     getClassDivValue: function() {
-        return $(".class-div").text();
+        
     },
     getTwoClassDivValue: function() {
-        return $(".second-class").text();
+        
     },
     getIDClassDivValue: function() {
-        return $("#class-id-div").text();
+        
     }
 };
 
