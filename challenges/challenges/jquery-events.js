5a6ffed5add4a53c8147a23c1b857fac7c32604d
Michael Fazio <mfazio@skylinetechnologies.com>
2016-06-08 15:10:50 -0500

    Remove answers.

--- challenges/challenges/jquery-events.js
+++ challenges/challenges/jquery-events.js
@@ -8,23 +8,7 @@ function swapColors() {
 
 module.exports = {
     assignEvents: function () {
-        $("#clickable-span").on('click', swapColors);
-
-        $("#hoverable-span")
-            .mouseover(function (e) {
-                $(this).css('color', 'white').css('background-color', 'blue');
-            })
-            .mouseout(function (e) {
-                $(this).css('color', 'black').css('background-color', 'white');
-            });
-
-        $(".hover-event-box")
-            .mouseover(function (e) {
-                $(this).css('background-color', 'green');
-            })
-            .mouseout(function (e) {
-                $(this).css('background-color', 'white');
-            });
+        
     }
 
 };
